4eb08eafecfa712c8742605607cb74ea77ad444d
OmarMahfoze17 <31227063+OmarMahfoze17@users.noreply.github.com>
2022-09-15 01:13:56 +0100

    Add files via upload

--- Particals_stat_total.m
+++ Particals_stat_total.m
@@ -4,78 +4,75 @@ close all
 
 
 
-fontSize=13;
+fontSize=16;
 SaveDir='/home/omar/WORK/results/Covid-19';
-
+%%%%%%%%%%%%%%%%%%%%%%%   NOTE %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
+%%%% Copy all the files in a subdirectory called ALL %%%%%%
+%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
 Dir{1}='/mnt/nvme1n1/WORK/Covid/Airbus/Data/AirBusProject/NoShieldsNoNozzles';
 Dir{2}='/mnt/nvme1n1/WORK/Covid/Airbus/Data/AirBusProject/NoShieldsWithNozzles';
 Dir{3}='/mnt/nvme1n1/WORK/Covid/Airbus/Data/AirBusProject/NoShieldsNoNozzles_OneInlet';
 Dir{4}='/mnt/nvme1n1/WORK/Covid/Airbus/Data/AirBusProject/NoShieldsWithNozzles_OneInlet';
 
+Color={'k','b','r'}
 
 %%% Get total number of particals
 tic
-nPartTotal=338000;
-nPartTotal=366000;
-partDimRank=1;
-% [nPartTotal,~]=get_nPart(Dir);
-for iDir=1:4;
-    for iPass=1:60;
-        [nP_pass{iPass}]=readDPM(fullfile(Dir{iDir},num2str(iPass,'passenger_%.2i.dpm')));
-        [nP_mth{iPass}]=readDPM(fullfile(Dir{iDir},num2str(iPass,'mouth_%.2i.dpm')));
-        nP_pass{iPass}=round(nP_pass{iPass}/nPartTotal*100,2);
-        nP_mth{iPass}=round(nP_mth{iPass}/nPartTotal*100,2);
-        
-    end
-    
+nPartTotal=368000;
+% nPartTotal=338000;
+
+
+szRng=[1]/10^6;
+maxHigth=[0.4 .9 1.5];
+minHigth=[-10 0.4 0.9];
+
+for iDir=1:length(Dir)
     figure
-    set(axes,'FontSize',fontSize,'TickLabelInterpreter','latex')
-    hold on; grid on; box on;
-    set(gcf, 'Units', 'Normalized', 'OuterPosition', [.3, 0.2, 0.7, 0.5]);
-    title(['$',Dir{iDir}(find(Dir{iDir}=='/',1,'last')+1:end),'$'],'Interpreter','Latex','FontSize',12)
-    dx=1;
-    
-    dy=.3;
-    xlim([0 dx*11])
-    ylim([dy dy*8.5])
-    iPass=0;
-    for I=1:10
-        dyy=0;
-        for J=1:6
-            iPass=iPass+1;
-            if J==4 ; dyy=dy*1; end
-            text(I*dx, dy*9- (J*dy+dyy), num2str(nP_pass{iPass}(partDimRank)),...
-                'Color','k','FontSize',fontSize,'Interpreter','Latex','BackgroundColor',[0 0 0 .1]);
-            
-            text(I*dx, dy*9- (J*dy+dyy)-dy/2, num2str(nP_mth{iPass}(partDimRank)),...
-                'Color','r','FontSize',fontSize,'Interpreter','Latex','BackgroundColor',[1 0 0 .1]);
-        end
+set(axes,'FontSize',fontSize,'TickLabelInterpreter','latex')
+hold on; grid on; box on;
+   
+    %%%%%%%%%%%%%%%%%%%%% PLOT All %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
+    for ISz=1:length(maxHigth);   
+        [nP_all,T_all,M_all]=get_nPart(fullfile(Dir{iDir},'ALL/*'),szRng,minHigth(ISz),maxHigth(ISz));
+        T_all{1}(end+1)=300;
+
+        myplot(T_all{1},[1:length(T_all{1})]/nPartTotal*100,Color{ISz},2,num2str([max(0,minHigth(ISz)),maxHigth(ISz)],'Y = %0.1f : %0.1f'),fontSize)
     end
-    exportgraphics(gca, fullfile(SaveDir,[Dir{iDir}(find(Dir{iDir}=='/',1,'last')+1:end),'.pdf']), 'ContentType', 'vector');
+
+xlim([0 300]);
+%         ylim([0 60])
+xlabel('Time(s)','Interpreter','Latex','FontSize',fontSize)
+ylabel('Deposition Fraction(\%)','Interpreter','Latex','FontSize',fontSize)
+
+legend('NumColumns',1,'Interpreter','Latex','FontSize',14,'location','best','EdgeColor',[1 1 1])
+annotation('textbox', [0.4 0.95 0.05 0.05],'String',[Dir{iDir}(find(Dir{iDir}=='/',1,'last')+1:end)],'Interpreter','Latex','FontSize',14);
+exportgraphics(gca, fullfile(SaveDir,['Total_',Dir{iDir}(find(Dir{iDir}=='/',1,'last')+1:end),'.pdf']), 'ContentType', 'vector');
 
 end
-toc
+%%%
+
+
 
 
 function myplot(x,y,Clr,wdth,name,fontSize)
 plot(x,y,Clr,'linewidth',wdth,'DisplayName',name)
-
-text(250, y(end)+0.5, num2str(y(end),'%3.1f\\%%'),...
-    'Color',Clr(end),'FontSize',fontSize,'Interpreter','Latex','BackgroundColor','none');
-
+try
+    text(250, y(end)+0.5, num2str(y(end),'%3.1f\\%%'),...
+        'Color',Clr(end),'FontSize',fontSize,'Interpreter','Latex','BackgroundColor','none');
+end
 end
 %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
-function [nPartTotal,time,MSS]=get_nPart(Dir,diamRng)
+function [nPartTotal,time,MSS,Loc]=get_nPart(Dir,diamRng,minHigth,maxHigth)
 fileinfo = dir(Dir);
 nPartTotal=[0 0 0 0 0];
 time=cell(size(diamRng));
 MSS=cell(size(diamRng));
-for I=1:length(fileinfo)
-    if fileinfo(I).isdir
+Loc=cell(length(diamRng));
+for J=1:length(fileinfo)
+    if fileinfo(J).isdir
         continue
     end
-    [nPart,T,M,dRng]=readDPM(fullfile(fileinfo(I).folder,fileinfo(I).name));
-    
+    [nPart,T,M,dRng,L]=readDPM(fullfile(fileinfo(J).folder,fileinfo(J).name));
     % error('vdsvs#')
     for I=1:length(dRng)
         II=find(diamRng==dRng(I));
@@ -83,17 +80,23 @@ for I=1:length(fileinfo)
         nPartTotal(II)=nPartTotal(II)+nPart(I);
         time{II}=[time{II};T{I}];
         MSS{II}=[MSS{II};M{I}];
+        Loc{II}=[Loc{II};L{I}];
+
     end
-    
+
 end
+
 for I=1:length(diamRng)
+
+    time{I}=time{I}(Loc{I}(:,2)>=minHigth & Loc{I}(:,2)<=maxHigth);
     time{I}=sort(time{I});
 end
 end
 
 %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
-function [nPart,T,M,dRng]=readDPM(fileName)
+function [nPart,T,M,dRng,L]=readDPM(fileName)
 % fileName
+fileName;
 fileID = fopen(fileName);
 
 Data = textscan(fileID,'%n %n %n %n %n %n %n %n %n %n %n %n %n %s %s',...
@@ -125,33 +128,36 @@ end
 
 %%%%%%%%%%%%%%%%% Separate Partical based on the partical size %%%%%%%%%%%%
 for nSize=1:length(dRng)
-    %     L{nSize}(:,1)=Data{2}(Data{8}==dRng(nSize),:);
-    %     L{nSize}(:,2)=Data{3}(Data{8}==dRng(nSize),:);
-    %     L{nSize}(:,3)=Data{4}(Data{8}==dRng(nSize),:);
+    L{nSize}(:,1)=Data{2}(Data{8}==dRng(nSize),:);
+    L{nSize}(:,2)=Data{3}(Data{8}==dRng(nSize),:);
+    L{nSize}(:,3)=Data{4}(Data{8}==dRng(nSize),:);
     %
     %     V{nSize}(:,1)=Data{5}(Data{8}==dRng(nSize),:);
     %     V{nSize}(:,2)=Data{6}(Data{8}==dRng(nSize),:);
     %     V{nSize}(:,3)=Data{7}(Data{8}==dRng(nSize),:);
-    
+
     D{nSize}(:,1)=Data{8}(Data{8}==dRng(nSize));
     T{nSize}(:,1)=Data{13}(Data{8}==dRng(nSize));
     M{nSize}(:,1)=Data{11}(Data{8}==dRng(nSize));
-    
+
     ID{nSize}(:,1)=Data{15}(Data{8}==dRng(nSize));
-    
+
     %     A=[1 2 3 3 5 0 0]
     %     [v,w]=unique(A)
     %     [v, w] = unique(ID{nSize}, 'stable' )
     %     duplicate_indices = setdiff( 1:numel(ID{nSize}), w )
     %     vsdvsv
-    
+
     %%%%%%%%%%%%%% Time Sort %%%%%%%%%%%%%%%%%%%%%%%%%
     [T{nSize},sortInd]=sort(T{nSize},'ascend');
     %     L{nSize}=L{nSize}(sortInd,:);
     %     V{nSize}=V{nSize}(sortInd,:);
     D{nSize}=D{nSize}(sortInd,:);
+    L{nSize}=L{nSize}(sortInd,:);
     M{nSize}=M{nSize}(sortInd,:);
     nPart(nSize)=length(T{nSize});
 end
 
 end
+
+
